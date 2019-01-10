470cfdf52f3753a9b71e8b017a9fccb3a4483f05
tomcyphilipkora <tomcyphilip567@gmaiol.com>
2019-01-11 00:51:56 +0530

    first commit

---
+++ ReminderApp/Home.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace ReminderApp
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.cbHour = new System.Windows.Forms.ComboBox();
+            this.cbMin = new System.Windows.Forms.ComboBox();
+            this.cbAMPM = new System.Windows.Forms.ComboBox();
+            this.lblRemHeading = new System.Windows.Forms.Label();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtHeading = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvReminderDetails = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReminderDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.ForeColor = System.Drawing.Color.Red;
+            this.lblHeading.Location = new System.Drawing.Point(12, 9);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(742, 36);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "REMINDER APP";
+            this.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnAddNew);
+            this.panel1.Controls.Add(this.txtBody);
+            this.panel1.Controls.Add(this.txtHeading);
+            this.panel1.Controls.Add(this.lblBody);
+            this.panel1.Controls.Add(this.lblRemHeading);
+            this.panel1.Controls.Add(this.cbAMPM);
+            this.panel1.Controls.Add(this.cbMin);
+            this.panel1.Controls.Add(this.cbHour);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Controls.Add(this.dtpDate);
+            this.panel1.Location = new System.Drawing.Point(18, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 161);
+            this.panel1.TabIndex = 1;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(91, 14);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(143, 20);
+            this.dtpDate.TabIndex = 0;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(55, 20);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Date";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(260, 20);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Time";
+            // 
+            // cbHour
+            // 
+            this.cbHour.FormattingEnabled = true;
+            this.cbHour.Location = new System.Drawing.Point(296, 12);
+            this.cbHour.Name = "cbHour";
+            this.cbHour.Size = new System.Drawing.Size(45, 21);
+            this.cbHour.TabIndex = 3;
+            // 
+            // cbMin
+            // 
+            this.cbMin.FormattingEnabled = true;
+            this.cbMin.Location = new System.Drawing.Point(347, 12);
+            this.cbMin.Name = "cbMin";
+            this.cbMin.Size = new System.Drawing.Size(45, 21);
+            this.cbMin.TabIndex = 4;
+            // 
+            // cbAMPM
+            // 
+            this.cbAMPM.FormattingEnabled = true;
+            this.cbAMPM.Items.AddRange(new object[] {
+            ""});
+            this.cbAMPM.Location = new System.Drawing.Point(398, 12);
+            this.cbAMPM.Name = "cbAMPM";
+            this.cbAMPM.Size = new System.Drawing.Size(45, 21);
+            this.cbAMPM.TabIndex = 5;
+            // 
+            // lblRemHeading
+            // 
+            this.lblRemHeading.AutoSize = true;
+            this.lblRemHeading.Location = new System.Drawing.Point(38, 51);
+            this.lblRemHeading.Name = "lblRemHeading";
+            this.lblRemHeading.Size = new System.Drawing.Size(47, 13);
+            this.lblRemHeading.TabIndex = 6;
+            this.lblRemHeading.Text = "Heading";
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Location = new System.Drawing.Point(54, 102);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(31, 13);
+            this.lblBody.TabIndex = 7;
+            this.lblBody.Text = "Body";
+            // 
+            // txtHeading
+            // 
+            this.txtHeading.Location = new System.Drawing.Point(92, 51);
+            this.txtHeading.Name = "txtHeading";
+            this.txtHeading.Size = new System.Drawing.Size(351, 20);
+            this.txtHeading.TabIndex = 8;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(92, 95);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(351, 54);
+            this.txtBody.TabIndex = 9;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(548, 28);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 10;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(548, 73);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(548, 117);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvReminderDetails
+            // 
+            this.dgvReminderDetails.AllowUserToAddRows = false;
+            this.dgvReminderDetails.AllowUserToDeleteRows = false;
+            this.dgvReminderDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReminderDetails.Location = new System.Drawing.Point(18, 262);
+            this.dgvReminderDetails.Name = "dgvReminderDetails";
+            this.dgvReminderDetails.ReadOnly = true;
+            this.dgvReminderDetails.Size = new System.Drawing.Size(736, 208);
+            this.dgvReminderDetails.TabIndex = 2;
+            this.dgvReminderDetails.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvReminderDetails_RowHeaderMouseClick);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 482);
+            this.Controls.Add(this.dgvReminderDetails);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReminderDetails)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.Label lblRemHeading;
+        private System.Windows.Forms.ComboBox cbAMPM;
+        private System.Windows.Forms.ComboBox cbMin;
+        private System.Windows.Forms.ComboBox cbHour;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtHeading;
+        private System.Windows.Forms.DataGridView dgvReminderDetails;
+    }
+}
---
+++ ReminderApp/Home.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReminderApp
+{
+    public partial class Home : Form
+    {
+        SqlConnection con = new SqlConnection("data source=DESKTOP-0M7JDPE;initial catalog=ReminderApp;integrated security=True;");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        int id = 0;
+
+        public Home()
+        {
+            InitializeComponent();
+            DisplayReminderData();
+            DataGridViewColumn c0 = dgvReminderDetails.Columns[0];
+            c0.Visible = false;
+            DataGridViewColumn c1 = dgvReminderDetails.Columns[1];
+            c1.Width = 150;
+            DataGridViewColumn c2 = dgvReminderDetails.Columns[2];
+            c2.Width = 243;
+            DataGridViewColumn c3 = dgvReminderDetails.Columns[3];
+            c3.Width = 150;
+            DataGridViewColumn c4 = dgvReminderDetails.Columns[4];
+            c4.Width = 150;
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(1);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                DisplayReminderPopup();
+            }, null, startTimeSpan, periodTimeSpan);
+        }
+
+        private void DisplayReminderPopup()
+        {
+            DateTime now = DateTime.Now;
+            string date = now.ToString("yyyy-MM-dd");
+            string time = now.Hour + ":" + now.Minute;
+            string msg = "", title = "";
+            con.Open();
+            cmd = new SqlCommand("select [ReminderHeading] AS Heading, [ReminderBody] AS Body from [dbo].[Reminder] WHERE RemDate = @date AND RemTime=@time", con);
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.Parameters.AddWithValue("@time", time);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                msg = reader["Body"].ToString();
+                title = reader["Heading"].ToString();
+            }
+
+            con.Close();
+            
+            MessageBox.Show(msg, title);
+        }
+
+        private void DisplayReminderData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select [ReminderID], [ReminderHeading] AS Heading, [ReminderBody] AS Body, [RemDate] AS Date, [RemTime] AS Time from [dbo].[Reminder]", con);
+            adapt.Fill(dt);
+            dgvReminderDetails.DataSource = dt;
+            con.Close();  
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                this.cbHour.Items.Add(i);
+            }
+            for (int i = 0; i < 60; i++)
+            {
+                this.cbMin.Items.Add(i);
+            }
+
+            this.cbAMPM.Items.Add("AM");
+            this.cbAMPM.Items.Add("PM");
+        }
+
+        private void dgvReminderDetails_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = Convert.ToInt32(dgvReminderDetails.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtHeading.Text = dgvReminderDetails.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtBody.Text = dgvReminderDetails.Rows[e.RowIndex].Cells[2].Value.ToString();
+            dtpDate.Value = Convert.ToDateTime(dgvReminderDetails.Rows[e.RowIndex].Cells[3].Value);
+            int hour = Convert.ToDateTime(dgvReminderDetails.Rows[e.RowIndex].Cells[4].Value.ToString()).Hour - 1;
+            int min = Convert.ToDateTime(dgvReminderDetails.Rows[e.RowIndex].Cells[4].Value.ToString()).Minute;
+            int amPmIndex = 1;
+
+            if (hour > 12)
+            {
+                hour = hour - 12;
+                amPmIndex = 2;
+            }
+            cbHour.SelectedIndex = hour == -1 ? 11 : hour;
+            cbMin.SelectedIndex = min;
+            cbAMPM.SelectedIndex = amPmIndex;
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                insertReminder();
+            }
+            else
+            {
+                MessageBox.Show("Please enter all value");
+            }
+        }
+
+        private void insertReminder()
+        {
+            string hour = "";
+            if (cbAMPM.Text == "PM")
+            {
+                hour = (Convert.ToInt32(cbHour.Text) + 12).ToString();
+            }
+            else
+            {
+                hour = cbHour.Text;
+            }
+            string time = hour + ":" + cbMin.Text;
+
+            cmd = new SqlCommand("insert into [dbo].[Reminder]([ReminderHeading],[ReminderBody],[RemDate],[RemTime]) values(@heading,@body,@date,@time)", con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@heading", txtHeading.Text);
+            cmd.Parameters.AddWithValue("@body", txtBody.Text);
+            cmd.Parameters.AddWithValue("@date", dtpDate.Value.ToString("yyyy-MM-dd"));
+            cmd.Parameters.AddWithValue("@time", time);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record Inserted Successfully");
+            DisplayReminderData();
+            ClearData(); 
+        }
+
+        private void ClearData()
+        {
+            cbHour.SelectedIndex = -1;
+            cbMin.SelectedIndex = -1;
+            cbAMPM.SelectedIndex = -1;
+            txtHeading.Text = "";
+            txtBody.Text = "";
+        }
+
+        private bool ValidateInput()
+        {
+            bool status = false, dateStatus = false, houratatus = false, minStatus = false, amPmStatus = false;
+            bool headingStatus = false, bodyStatus = false;
+
+            if (dtpDate.Value.Date != null)
+            {
+                dateStatus = true;
+            }
+            if (cbHour.SelectedIndex != -1)
+            {
+                houratatus = true;
+            }
+            if (cbMin.SelectedIndex != -1)
+            {
+                minStatus = true;
+            }
+            if (cbAMPM.SelectedIndex != -1)
+            {
+                amPmStatus = true;
+            }
+            if (txtHeading.Text != null)
+            {
+                headingStatus = true;
+            }
+            if (txtBody.Text != null)
+            {
+                bodyStatus = true;
+            }
+
+            if (dateStatus && houratatus && minStatus && amPmStatus && headingStatus && bodyStatus)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            return status;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (id == 0)
+            {
+                MessageBox.Show("Please Select Record to Update");
+            }
+            else if (ValidateInput())
+            {
+                string hour = "";
+                if (cbAMPM.Text == "PM")
+                {
+                    hour = (Convert.ToInt32(cbHour.Text) + 12).ToString();
+                }
+                if (cbAMPM.Text == "AM" && Convert.ToInt32(cbHour.Text)==12)
+                {
+                    hour = (Convert.ToInt32(cbHour.Text) - 12).ToString();
+                }
+                else
+                {
+                    hour = cbHour.Text;
+                }
+                string time = hour + ":" + cbMin.Text;
+
+                cmd = new SqlCommand("update [dbo].[Reminder] set [ReminderHeading]=@heading,[ReminderBody]=@body,[RemDate]=@date,[RemTime]=@time WHERE [ReminderID]=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@heading", txtHeading.Text);
+                cmd.Parameters.AddWithValue("@body", txtBody.Text);
+                cmd.Parameters.AddWithValue("@date", dtpDate.Value.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@time", time);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Updated Successfully");
+                DisplayReminderData();
+                ClearData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (id != 0)
+            {
+                DialogResult res = MessageBox.Show("Are you sure you want to delete?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (res == DialogResult.Yes)
+                {
+                    cmd = new SqlCommand("delete from [dbo].[Reminder] where ReminderID=@id", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record Deleted Successfully!");
+                    DisplayReminderData();
+                    ClearData();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }  
+        } 
+
+    }
+}
